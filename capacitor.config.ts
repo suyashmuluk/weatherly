00f4c0dcfb94d1c04eeae060a91c523269ac2da4
Suyash <suyash.muluk@exportify.in>
2021-10-13 15:09:29 +0530

    changed settings of dist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'weatherly',
-  webDir: 'dist',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
